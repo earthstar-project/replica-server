12c57b35644b2691eca175716167b1d73ca5730c
Sam Gwilym <gwilym@me.com>
2022-04-13 12:50:03 +0000

    Update for Earthstar v9

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as Earthstar from "https://deno.land/x/earthstar@v8.3.0/mod.ts";
-export * as Rpc from "https://deno.land/x/earthstar_streaming_rpc@3.2.3/mod.ts";
+export * as Earthstar from "https://deno.land/x/earthstar@v9.0.0/mod.ts";
+export * as Rpc from "https://deno.land/x/earthstar_streaming_rpc@v4.0.3/mod.ts";
